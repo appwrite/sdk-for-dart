e1569354ae0e65646dee395b47261125e2e163d5
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-10-18 09:09:28 +0200

    feat(version): support 0.11.0

--- lib/services/teams.dart
+++ lib/services/teams.dart
@@ -150,9 +150,9 @@ class Teams extends Service {
 
         final Map<String, dynamic> params = {
             'email': email,
-            'name': name,
             'roles': roles,
             'url': url,
+            'name': name,
         };
 
         final Map<String, String> headers = {
