610163bfbcc8eba431100942660cef68e3e07954
Damodar Lohani <dlohani48@gmail.com>
2022-04-12 11:10:56 +0545

    latest fixes for attributes

--- lib/src/client_io.dart
+++ lib/src/client_io.dart
@@ -35,7 +35,7 @@ class ClientIO extends ClientBase with ClientMixin {
     _endPoint = endPoint;
     _headers = {
       'content-type': 'application/json',
-      'x-sdk-version': 'appwrite:dart:4.0.1',
+      'x-sdk-version': 'appwrite:dart:4.0.2',
       'X-Appwrite-Response-Format' : '0.13.0',
     };
 
