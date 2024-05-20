061ecd601a183dc6c8b1a40d0fcdfd7e023ae290
stnguyen90 <stnguyen90@users.noreply.github.com>
2024-05-20 23:58:44 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/attribute_float.dart
+++ lib/src/models/attribute_float.dart
@@ -2,62 +2,70 @@ part of '../../models.dart';
 
 /// AttributeFloat
 class AttributeFloat implements Model {
-    /// Attribute Key.
-    final String key;
-    /// Attribute type.
-    final String type;
-    /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-    final String status;
-    /// Error message. Displays error generated on failure of creating or deleting an attribute.
-    final String error;
-    /// Is attribute required?
-    final bool xrequired;
-    /// Is attribute an array?
-    final bool? array;
-    /// Minimum value to enforce for new documents.
-    final double? min;
-    /// Maximum value to enforce for new documents.
-    final double? max;
-    /// Default value for attribute when not provided. Cannot be set when attribute is required.
-    final double? xdefault;
+  /// Attribute Key.
+  final String key;
 
-    AttributeFloat({
-        required this.key,
-        required this.type,
-        required this.status,
-        required this.error,
-        required this.xrequired,
-        this.array,
-        this.min,
-        this.max,
-        this.xdefault,
-    });
+  /// Attribute type.
+  final String type;
 
-    factory AttributeFloat.fromMap(Map<String, dynamic> map) {
-        return AttributeFloat(
-            key: map['key'].toString(),
-            type: map['type'].toString(),
-            status: map['status'].toString(),
-            error: map['error'].toString(),
-            xrequired: map['required'],
-            array: map['array'],
-            min: map['min']?.toDouble(),
-            max: map['max']?.toDouble(),
-            xdefault: map['default']?.toDouble(),
-        );
-    }
+  /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+  final String status;
 
-    Map<String, dynamic> toMap() {
-        return {
-            "key": key,
-            "type": type,
-            "status": status,
-            "error": error,
-            "required": xrequired,
-            "array": array,
-            "min": min,
-            "max": max,
-            "default": xdefault,
-        };
-    }
+  /// Error message. Displays error generated on failure of creating or deleting an attribute.
+  final String error;
+
+  /// Is attribute required?
+  final bool xrequired;
+
+  /// Is attribute an array?
+  final bool? array;
+
+  /// Minimum value to enforce for new documents.
+  final double? min;
+
+  /// Maximum value to enforce for new documents.
+  final double? max;
+
+  /// Default value for attribute when not provided. Cannot be set when attribute is required.
+  final double? xdefault;
+
+  AttributeFloat({
+    required this.key,
+    required this.type,
+    required this.status,
+    required this.error,
+    required this.xrequired,
+    this.array,
+    this.min,
+    this.max,
+    this.xdefault,
+  });
+
+  factory AttributeFloat.fromMap(Map<String, dynamic> map) {
+    return AttributeFloat(
+      key: map['key'].toString(),
+      type: map['type'].toString(),
+      status: map['status'].toString(),
+      error: map['error'].toString(),
+      xrequired: map['required'],
+      array: map['array'],
+      min: map['min']?.toDouble(),
+      max: map['max']?.toDouble(),
+      xdefault: map['default']?.toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "key": key,
+      "type": type,
+      "status": status,
+      "error": error,
+      "required": xrequired,
+      "array": array,
+      "min": min,
+      "max": max,
+      "default": xdefault,
+    };
+  }
 }
--- lib/src/models/runtime.dart
+++ lib/src/models/runtime.dart
@@ -2,52 +2,58 @@ part of '../../models.dart';
 
 /// Runtime
 class Runtime implements Model {
-    /// Runtime ID.
-    final String $id;
-    /// Runtime Name.
-    final String name;
-    /// Runtime version.
-    final String version;
-    /// Base Docker image used to build the runtime.
-    final String base;
-    /// Image name of Docker Hub.
-    final String image;
-    /// Name of the logo image.
-    final String logo;
-    /// List of supported architectures.
-    final List supports;
+  /// Runtime ID.
+  final String $id;
 
-    Runtime({
-        required this.$id,
-        required this.name,
-        required this.version,
-        required this.base,
-        required this.image,
-        required this.logo,
-        required this.supports,
-    });
+  /// Runtime Name.
+  final String name;
 
-    factory Runtime.fromMap(Map<String, dynamic> map) {
-        return Runtime(
-            $id: map['\$id'].toString(),
-            name: map['name'].toString(),
-            version: map['version'].toString(),
-            base: map['base'].toString(),
-            image: map['image'].toString(),
-            logo: map['logo'].toString(),
-            supports: map['supports'] ?? [],
-        );
-    }
+  /// Runtime version.
+  final String version;
 
-    Map<String, dynamic> toMap() {
-        return {
-            "\$id": $id,
-            "name": name,
-            "version": version,
-            "base": base,
-            "image": image,
-            "logo": logo,
-            "supports": supports,
-        };
-    }
+  /// Base Docker image used to build the runtime.
+  final String base;
+
+  /// Image name of Docker Hub.
+  final String image;
+
+  /// Name of the logo image.
+  final String logo;
+
+  /// List of supported architectures.
+  final List supports;
+
+  Runtime({
+    required this.$id,
+    required this.name,
+    required this.version,
+    required this.base,
+    required this.image,
+    required this.logo,
+    required this.supports,
+  });
+
+  factory Runtime.fromMap(Map<String, dynamic> map) {
+    return Runtime(
+      $id: map['\$id'].toString(),
+      name: map['name'].toString(),
+      version: map['version'].toString(),
+      base: map['base'].toString(),
+      image: map['image'].toString(),
+      logo: map['logo'].toString(),
+      supports: map['supports'] ?? [],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "\$id": $id,
+      "name": name,
+      "version": version,
+      "base": base,
+      "image": image,
+      "logo": logo,
+      "supports": supports,
+    };
+  }
 }
