8b96cd1584443bc57761deab3604ab61f6ab724e
root <christyjacob4@gmail.com>
2024-08-23 10:51:11 +0000

    chore: updates for next rc release

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Add this to your package's `pubspec.yaml` file:
 
 ```yml
 dependencies:
-  dart_appwrite: ^12.0.0-rc.3
+  dart_appwrite: ^12.0.0-rc.4
 ```
 
 You can install packages from the command line:
--- lib/src/models/index.dart
+++ lib/src/models/index.dart
@@ -2,52 +2,47 @@ part of '../../models.dart';
 
 /// Index
 class Index implements Model {
-  /// Index Key.
-  final String key;
+    /// Index Key.
+    final String key;
+    /// Index type.
+    final String type;
+    /// Index status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+    final String status;
+    /// Error message. Displays error generated on failure of creating or deleting an index.
+    final String error;
+    /// Index attributes.
+    final List attributes;
+    /// Index orders.
+    final List? orders;
 
-  /// Index type.
-  final String type;
+    Index({
+        required this.key,
+        required this.type,
+        required this.status,
+        required this.error,
+        required this.attributes,
+        this.orders,
+    });
 
-  /// Index status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-  final String status;
+    factory Index.fromMap(Map<String, dynamic> map) {
+        return Index(
+            key: map['key'].toString(),
+            type: map['type'].toString(),
+            status: map['status'].toString(),
+            error: map['error'].toString(),
+            attributes: map['attributes'] ?? [],
+            orders: map['orders'],
+        );
+    }
 
-  /// Error message. Displays error generated on failure of creating or deleting an index.
-  final String error;
-
-  /// Index attributes.
-  final List attributes;
-
-  /// Index orders.
-  final List? orders;
-
-  Index({
-    required this.key,
-    required this.type,
-    required this.status,
-    required this.error,
-    required this.attributes,
-    this.orders,
-  });
-
-  factory Index.fromMap(Map<String, dynamic> map) {
-    return Index(
-      key: map['key'].toString(),
-      type: map['type'].toString(),
-      status: map['status'].toString(),
-      error: map['error'].toString(),
-      attributes: map['attributes'] ?? [],
-      orders: map['orders'],
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      "key": key,
-      "type": type,
-      "status": status,
-      "error": error,
-      "attributes": attributes,
-      "orders": orders,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "key": key,
+            "type": type,
+            "status": status,
+            "error": error,
+            "attributes": attributes,
+            "orders": orders,
+        };
+    }
 }
