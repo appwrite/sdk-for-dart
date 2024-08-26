526331ac3e2835c16c62c540132d51261476605e
christyjacob4 <christyjacob4@users.noreply.github.com>
2024-08-26 16:35:16 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/messaging_provider_type.dart
+++ lib/src/enums/messaging_provider_type.dart
@@ -1,15 +1,13 @@
 part of '../../enums.dart';
 
 enum MessagingProviderType {
-    email(value: 'email'),
-    sms(value: 'sms'),
-    push(value: 'push');
+  email(value: 'email'),
+  sms(value: 'sms'),
+  push(value: 'push');
 
-    const MessagingProviderType({
-        required this.value
-    });
+  const MessagingProviderType({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
