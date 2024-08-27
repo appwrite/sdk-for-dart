13594fc90ae8d284c2c506a976d9af19c6ce5ec6
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-27 15:27:32 +0200

    fix syntax

--- lib/src/models/attribute_datetime.dart
+++ lib/src/models/attribute_datetime.dart
@@ -2,64 +2,57 @@ part of '../../models.dart';
 
 /// AttributeDatetime
 class AttributeDatetime implements Model {
-  /// Attribute Key.
-  final String key;
+    /// Attribute Key.
+    final String key;
+    /// Attribute type.
+    final String type;
+    /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+    final String status;
+    /// Error message. Displays error generated on failure of creating or deleting an attribute.
+    final String error;
+    /// Is attribute required?
+    final bool xrequired;
+    /// Is attribute an array?
+    final bool? array;
+    /// ISO 8601 format.
+    final String format;
+    /// Default value for attribute when not provided. Only null is optional
+    final String? xdefault;
 
-  /// Attribute type.
-  final String type;
+    AttributeDatetime({
+        required this.key,
+        required this.type,
+        required this.status,
+        required this.error,
+        required this.xrequired,
+        this.array,
+        required this.format,
+        this.xdefault,
+    });
 
-  /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-  final String status;
+    factory AttributeDatetime.fromMap(Map<String, dynamic> map) {
+        return AttributeDatetime(
+            key: map['key'].toString(),
+            type: map['type'].toString(),
+            status: map['status'].toString(),
+            error: map['error'].toString(),
+            xrequired: map['required'],
+            array: map['array'],
+            format: map['format'].toString(),
+            xdefault: map['default']?.toString(),
+        );
+    }
 
-  /// Error message. Displays error generated on failure of creating or deleting an attribute.
-  final String error;
-
-  /// Is attribute required?
-  final bool xrequired;
-
-  /// Is attribute an array?
-  final bool? array;
-
-  /// ISO 8601 format.
-  final String format;
-
-  /// Default value for attribute when not provided. Only null is optional
-  final String? xdefault;
-
-  AttributeDatetime({
-    required this.key,
-    required this.type,
-    required this.status,
-    required this.error,
-    required this.xrequired,
-    this.array,
-    required this.format,
-    this.xdefault,
-  });
-
-  factory AttributeDatetime.fromMap(Map<String, dynamic> map) {
-    return AttributeDatetime(
-      key: map['key'].toString(),
-      type: map['type'].toString(),
-      status: map['status'].toString(),
-      error: map['error'].toString(),
-      xrequired: map['required'],
-      array: map['array'],
-      format: map['format'].toString(),
-      xdefault: map['default']?.toString(),
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      "key": key,
-      "type": type,
-      "status": status,
-      "error": error,
-      "required": xrequired,
-      "array": array,
-      "format": format,
-      "default": xdefault,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "key": key,
+            "type": type,
+            "status": status,
+            "error": error,
+            "required": xrequired,
+            "array": array,
+            "format": format,
+            "default": xdefault,
+        };
+    }
 }
