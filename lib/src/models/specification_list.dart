19465054eda5dca98c4fd1b2265c9ef1b40db003
christyjacob4 <christyjacob4@users.noreply.github.com>
2024-08-27 11:18:23 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/specification_list.dart
+++ lib/src/models/specification_list.dart
@@ -2,27 +2,29 @@ part of '../../models.dart';
 
 /// Specifications List
 class SpecificationList implements Model {
-    /// Total number of specifications documents that matched your query.
-    final int total;
-    /// List of specifications.
-    final List<Specification> specifications;
+  /// Total number of specifications documents that matched your query.
+  final int total;
 
-    SpecificationList({
-        required this.total,
-        required this.specifications,
-    });
+  /// List of specifications.
+  final List<Specification> specifications;
 
-    factory SpecificationList.fromMap(Map<String, dynamic> map) {
-        return SpecificationList(
-            total: map['total'],
-            specifications: List<Specification>.from(map['specifications'].map((p) => Specification.fromMap(p))),
-        );
-    }
+  SpecificationList({
+    required this.total,
+    required this.specifications,
+  });
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "specifications": specifications.map((p) => p.toMap()).toList(),
-        };
-    }
+  factory SpecificationList.fromMap(Map<String, dynamic> map) {
+    return SpecificationList(
+      total: map['total'],
+      specifications: List<Specification>.from(
+          map['specifications'].map((p) => Specification.fromMap(p))),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "total": total,
+      "specifications": specifications.map((p) => p.toMap()).toList(),
+    };
+  }
 }
