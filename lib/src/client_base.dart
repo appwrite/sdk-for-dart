87e6090d1d56d43de1bafd747f8ffe7cc7d7cddc
root <chiragaggarwal5k@gmail.com>
2025-03-06 17:29:52 +0000

    chore: regenerate sdk

--- lib/src/client_base.dart
+++ lib/src/client_base.dart
@@ -2,25 +2,21 @@ import 'response.dart';
 import 'client.dart';
 import 'enums.dart';
 
-abstract class ClientBase implements Client {
+abstract class ClientBase implements Client {  
   /// Your project ID
   @override
   ClientBase setProject(value);
-
   /// Your secret API key
   @override
   ClientBase setKey(value);
-
   /// Your secret JSON Web Token
   @override
   ClientBase setJWT(value);
   @override
   ClientBase setLocale(value);
-
   /// The user session to authenticate with
   @override
   ClientBase setSession(value);
-
   /// The user agent string of the client that made the request
   @override
   ClientBase setForwardedUserAgent(value);
@@ -35,7 +31,15 @@ abstract class ClientBase implements Client {
   ClientBase addHeader(String key, String value);
 
   @override
-  Future<String> ping();
+  Future<String> ping() async {
+    final String apiPath = '/ping';
+    final response = await call(
+      HttpMethod.get,
+      path: apiPath,
+      responseType: ResponseType.plain,
+    );
+    return response.data;
+  }
 
   @override
   Future<Response> call(
