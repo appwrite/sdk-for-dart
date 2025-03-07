02400d5e08b77dee8ea3a8278e7a3ca240d171cc
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-03-07 04:09:00 +0000

    Commit from GitHub Actions (Format and push)

--- lib/services/graphql.dart
+++ lib/services/graphql.dart
@@ -3,49 +3,47 @@ part of '../dart_appwrite.dart';
 /// The GraphQL API allows you to query and mutate your Appwrite server using
 /// GraphQL.
 class Graphql extends Service {
-    Graphql(super.client);
+  Graphql(super.client);
 
-    /// Execute a GraphQL mutation.
-    Future query({required Map query}) async {
-        final String apiPath = '/graphql';
+  /// Execute a GraphQL mutation.
+  Future query({required Map query}) async {
+    final String apiPath = '/graphql';
 
-        final Map<String, dynamic> apiParams = {
-            
-            'query': query,
+    final Map<String, dynamic> apiParams = {'query': query};
 
-        };
+    final Map<String, String> apiHeaders = {
+      'x-sdk-graphql': 'true',
+      'content-type': 'application/json',
+    };
 
-        final Map<String, String> apiHeaders = {
-            'x-sdk-graphql': 'true',
-'content-type': 'application/json',
+    final res = await client.call(
+      HttpMethod.post,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        };
+    return res.data;
+  }
 
-        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
+  /// Execute a GraphQL mutation.
+  Future mutation({required Map query}) async {
+    final String apiPath = '/graphql/mutation';
 
-        return  res.data;
+    final Map<String, dynamic> apiParams = {'query': query};
 
-    }
+    final Map<String, String> apiHeaders = {
+      'x-sdk-graphql': 'true',
+      'content-type': 'application/json',
+    };
 
-    /// Execute a GraphQL mutation.
-    Future mutation({required Map query}) async {
-        final String apiPath = '/graphql/mutation';
+    final res = await client.call(
+      HttpMethod.post,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        final Map<String, dynamic> apiParams = {
-            
-            'query': query,
-
-        };
-
-        final Map<String, String> apiHeaders = {
-            'x-sdk-graphql': 'true',
-'content-type': 'application/json',
-
-        };
-
-        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
-
-        return  res.data;
-
-    }
+    return res.data;
+  }
 }
