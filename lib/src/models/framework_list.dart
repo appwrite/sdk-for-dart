8513591c28ee5e73871d41af473f13777f979ad2
root <chiragaggarwal5k@gmail.com>
2025-05-18 08:36:01 +0000

    chore: add changelog

--- lib/src/models/framework_list.dart
+++ lib/src/models/framework_list.dart
@@ -2,27 +2,28 @@ part of '../../models.dart';
 
 /// Frameworks List
 class FrameworkList implements Model {
-  /// Total number of frameworks documents that matched your query.
-  final int total;
+    /// Total number of frameworks documents that matched your query.
+    final int total;
 
-  /// List of frameworks.
-  final List<Framework> frameworks;
+    /// List of frameworks.
+    final List<Framework> frameworks;
 
-  FrameworkList({required this.total, required this.frameworks});
+    FrameworkList({
+        required this.total,
+        required this.frameworks,
+    });
 
-  factory FrameworkList.fromMap(Map<String, dynamic> map) {
-    return FrameworkList(
-      total: map['total'],
-      frameworks: List<Framework>.from(
-        map['frameworks'].map((p) => Framework.fromMap(p)),
-      ),
-    );
-  }
+    factory FrameworkList.fromMap(Map<String, dynamic> map) {
+        return FrameworkList(
+            total: map['total'],
+            frameworks: List<Framework>.from(map['frameworks'].map((p) => Framework.fromMap(p))),
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {
-      "total": total,
-      "frameworks": frameworks.map((p) => p.toMap()).toList(),
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "frameworks": frameworks.map((p) => p.toMap()).toList(),
+        };
+    }
 }
