779a1f1a496a97bde0e789ccf12a1676e1f44951
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-07-23 04:21:14 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/framework_adapter.dart
+++ lib/src/models/framework_adapter.dart
@@ -2,46 +2,46 @@ part of '../../models.dart';
 
 /// Framework Adapter
 class FrameworkAdapter implements Model {
-    /// Adapter key.
-    final String key;
+  /// Adapter key.
+  final String key;
 
-    /// Default command to download dependencies.
-    final String installCommand;
+  /// Default command to download dependencies.
+  final String installCommand;
 
-    /// Default command to build site into output directory.
-    final String buildCommand;
+  /// Default command to build site into output directory.
+  final String buildCommand;
 
-    /// Default output directory of build.
-    final String outputDirectory;
+  /// Default output directory of build.
+  final String outputDirectory;
 
-    /// Name of fallback file to use instead of 404 page. If null, Appwrite 404 page will be displayed.
-    final String fallbackFile;
+  /// Name of fallback file to use instead of 404 page. If null, Appwrite 404 page will be displayed.
+  final String fallbackFile;
 
-    FrameworkAdapter({
-        required this.key,
-        required this.installCommand,
-        required this.buildCommand,
-        required this.outputDirectory,
-        required this.fallbackFile,
-    });
+  FrameworkAdapter({
+    required this.key,
+    required this.installCommand,
+    required this.buildCommand,
+    required this.outputDirectory,
+    required this.fallbackFile,
+  });
 
-    factory FrameworkAdapter.fromMap(Map<String, dynamic> map) {
-        return FrameworkAdapter(
-            key: map['key'].toString(),
-            installCommand: map['installCommand'].toString(),
-            buildCommand: map['buildCommand'].toString(),
-            outputDirectory: map['outputDirectory'].toString(),
-            fallbackFile: map['fallbackFile'].toString(),
-        );
-    }
+  factory FrameworkAdapter.fromMap(Map<String, dynamic> map) {
+    return FrameworkAdapter(
+      key: map['key'].toString(),
+      installCommand: map['installCommand'].toString(),
+      buildCommand: map['buildCommand'].toString(),
+      outputDirectory: map['outputDirectory'].toString(),
+      fallbackFile: map['fallbackFile'].toString(),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "key": key,
-            "installCommand": installCommand,
-            "buildCommand": buildCommand,
-            "outputDirectory": outputDirectory,
-            "fallbackFile": fallbackFile,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "key": key,
+      "installCommand": installCommand,
+      "buildCommand": buildCommand,
+      "outputDirectory": outputDirectory,
+      "fallbackFile": fallbackFile,
+    };
+  }
 }
