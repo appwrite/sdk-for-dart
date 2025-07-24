7acbd6c7b904cea83511d92396cdaabc655d86cf
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-07-24 06:35:32 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/topic_list.dart
+++ lib/src/models/topic_list.dart
@@ -2,28 +2,22 @@ part of '../../models.dart';
 
 /// Topic list
 class TopicList implements Model {
-    /// Total number of topics documents that matched your query.
-    final int total;
+  /// Total number of topics documents that matched your query.
+  final int total;
 
-    /// List of topics.
-    final List<Topic> topics;
+  /// List of topics.
+  final List<Topic> topics;
 
-    TopicList({
-        required this.total,
-        required this.topics,
-    });
+  TopicList({required this.total, required this.topics});
 
-    factory TopicList.fromMap(Map<String, dynamic> map) {
-        return TopicList(
-            total: map['total'],
-            topics: List<Topic>.from(map['topics'].map((p) => Topic.fromMap(p))),
-        );
-    }
+  factory TopicList.fromMap(Map<String, dynamic> map) {
+    return TopicList(
+      total: map['total'],
+      topics: List<Topic>.from(map['topics'].map((p) => Topic.fromMap(p))),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "topics": topics.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"total": total, "topics": topics.map((p) => p.toMap()).toList()};
+  }
 }
