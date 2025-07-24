ab49806888519e90630de8d3f574582cc0f964d5
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-07-24 06:44:48 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/resource_token_list.dart
+++ lib/src/models/resource_token_list.dart
@@ -2,28 +2,24 @@ part of '../../models.dart';
 
 /// Resource Tokens List
 class ResourceTokenList implements Model {
-    /// Total number of tokens documents that matched your query.
-    final int total;
+  /// Total number of tokens documents that matched your query.
+  final int total;
 
-    /// List of tokens.
-    final List<ResourceToken> tokens;
+  /// List of tokens.
+  final List<ResourceToken> tokens;
 
-    ResourceTokenList({
-        required this.total,
-        required this.tokens,
-    });
+  ResourceTokenList({required this.total, required this.tokens});
 
-    factory ResourceTokenList.fromMap(Map<String, dynamic> map) {
-        return ResourceTokenList(
-            total: map['total'],
-            tokens: List<ResourceToken>.from(map['tokens'].map((p) => ResourceToken.fromMap(p))),
-        );
-    }
+  factory ResourceTokenList.fromMap(Map<String, dynamic> map) {
+    return ResourceTokenList(
+      total: map['total'],
+      tokens: List<ResourceToken>.from(
+        map['tokens'].map((p) => ResourceToken.fromMap(p)),
+      ),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "tokens": tokens.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"total": total, "tokens": tokens.map((p) => p.toMap()).toList()};
+  }
 }
