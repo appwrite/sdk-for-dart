4b6f8ba5f433d0f6d082e65e50e8d8a36bf8fd03
abnegate <abnegate@users.noreply.github.com>
2025-08-20 06:40:01 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/attribute_string.dart
+++ lib/src/models/attribute_string.dart
@@ -2,82 +2,82 @@ part of '../../models.dart';
 
 /// AttributeString
 class AttributeString implements Model {
-    /// Attribute Key.
-    final String key;
+  /// Attribute Key.
+  final String key;
 
-    /// Attribute type.
-    final String type;
+  /// Attribute type.
+  final String type;
 
-    /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-    final String status;
+  /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+  final String status;
 
-    /// Error message. Displays error generated on failure of creating or deleting an attribute.
-    final String error;
+  /// Error message. Displays error generated on failure of creating or deleting an attribute.
+  final String error;
 
-    /// Is attribute required?
-    final bool xrequired;
+  /// Is attribute required?
+  final bool xrequired;
 
-    /// Is attribute an array?
-    final bool? array;
+  /// Is attribute an array?
+  final bool? array;
 
-    /// Attribute creation date in ISO 8601 format.
-    final String $createdAt;
+  /// Attribute creation date in ISO 8601 format.
+  final String $createdAt;
 
-    /// Attribute update date in ISO 8601 format.
-    final String $updatedAt;
+  /// Attribute update date in ISO 8601 format.
+  final String $updatedAt;
 
-    /// Attribute size.
-    final int size;
+  /// Attribute size.
+  final int size;
 
-    /// Default value for attribute when not provided. Cannot be set when attribute is required.
-    final String? xdefault;
+  /// Default value for attribute when not provided. Cannot be set when attribute is required.
+  final String? xdefault;
 
-    /// Defines whether this attribute is encrypted or not.
-    final bool? encrypt;
+  /// Defines whether this attribute is encrypted or not.
+  final bool? encrypt;
 
-    AttributeString({
-        required this.key,
-        required this.type,
-        required this.status,
-        required this.error,
-        required this.xrequired,
-        this.array,
-        required this.$createdAt,
-        required this.$updatedAt,
-        required this.size,
-        this.xdefault,
-        this.encrypt,
-    });
+  AttributeString({
+    required this.key,
+    required this.type,
+    required this.status,
+    required this.error,
+    required this.xrequired,
+    this.array,
+    required this.$createdAt,
+    required this.$updatedAt,
+    required this.size,
+    this.xdefault,
+    this.encrypt,
+  });
 
-    factory AttributeString.fromMap(Map<String, dynamic> map) {
-        return AttributeString(
-            key: map['key'].toString(),
-            type: map['type'].toString(),
-            status: map['status'].toString(),
-            error: map['error'].toString(),
-            xrequired: map['required'],
-            array: map['array'],
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            size: map['size'],
-            xdefault: map['default']?.toString(),
-            encrypt: map['encrypt'],
-        );
-    }
+  factory AttributeString.fromMap(Map<String, dynamic> map) {
+    return AttributeString(
+      key: map['key'].toString(),
+      type: map['type'].toString(),
+      status: map['status'].toString(),
+      error: map['error'].toString(),
+      xrequired: map['required'],
+      array: map['array'],
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      size: map['size'],
+      xdefault: map['default']?.toString(),
+      encrypt: map['encrypt'],
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "key": key,
-            "type": type,
-            "status": status,
-            "error": error,
-            "required": xrequired,
-            "array": array,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "size": size,
-            "default": xdefault,
-            "encrypt": encrypt,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "key": key,
+      "type": type,
+      "status": status,
+      "error": error,
+      "required": xrequired,
+      "array": array,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "size": size,
+      "default": xdefault,
+      "encrypt": encrypt,
+    };
+  }
 }
