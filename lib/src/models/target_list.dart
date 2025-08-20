da30c6d4159d53bd85398cbab325f1b16ae5635d
abnegate <abnegate@users.noreply.github.com>
2025-08-20 06:30:28 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/target_list.dart
+++ lib/src/models/target_list.dart
@@ -2,28 +2,22 @@ part of '../../models.dart';
 
 /// Target list
 class TargetList implements Model {
-    /// Total number of targets rows that matched your query.
-    final int total;
+  /// Total number of targets rows that matched your query.
+  final int total;
 
-    /// List of targets.
-    final List<Target> targets;
+  /// List of targets.
+  final List<Target> targets;
 
-    TargetList({
-        required this.total,
-        required this.targets,
-    });
+  TargetList({required this.total, required this.targets});
 
-    factory TargetList.fromMap(Map<String, dynamic> map) {
-        return TargetList(
-            total: map['total'],
-            targets: List<Target>.from(map['targets'].map((p) => Target.fromMap(p))),
-        );
-    }
+  factory TargetList.fromMap(Map<String, dynamic> map) {
+    return TargetList(
+      total: map['total'],
+      targets: List<Target>.from(map['targets'].map((p) => Target.fromMap(p))),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "targets": targets.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"total": total, "targets": targets.map((p) => p.toMap()).toList()};
+  }
 }
