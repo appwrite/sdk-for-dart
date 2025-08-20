95e352b9bc2d9eb8d887076e163dd2a893c3c835
abnegate <abnegate@users.noreply.github.com>
2025-08-20 06:26:48 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/database_list.dart
+++ lib/src/models/database_list.dart
@@ -2,28 +2,27 @@ part of '../../models.dart';
 
 /// Databases List
 class DatabaseList implements Model {
-    /// Total number of databases rows that matched your query.
-    final int total;
+  /// Total number of databases rows that matched your query.
+  final int total;
 
-    /// List of databases.
-    final List<Database> databases;
+  /// List of databases.
+  final List<Database> databases;
 
-    DatabaseList({
-        required this.total,
-        required this.databases,
-    });
+  DatabaseList({required this.total, required this.databases});
 
-    factory DatabaseList.fromMap(Map<String, dynamic> map) {
-        return DatabaseList(
-            total: map['total'],
-            databases: List<Database>.from(map['databases'].map((p) => Database.fromMap(p))),
-        );
-    }
+  factory DatabaseList.fromMap(Map<String, dynamic> map) {
+    return DatabaseList(
+      total: map['total'],
+      databases: List<Database>.from(
+        map['databases'].map((p) => Database.fromMap(p)),
+      ),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "databases": databases.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "total": total,
+      "databases": databases.map((p) => p.toMap()).toList(),
+    };
+  }
 }
--- lib/src/models/table_list.dart
+++ lib/src/models/table_list.dart
@@ -2,28 +2,22 @@ part of '../../models.dart';
 
 /// Tables List
 class TableList implements Model {
-    /// Total number of tables rows that matched your query.
-    final int total;
+  /// Total number of tables rows that matched your query.
+  final int total;
 
-    /// List of tables.
-    final List<Table> tables;
+  /// List of tables.
+  final List<Table> tables;
 
-    TableList({
-        required this.total,
-        required this.tables,
-    });
+  TableList({required this.total, required this.tables});
 
-    factory TableList.fromMap(Map<String, dynamic> map) {
-        return TableList(
-            total: map['total'],
-            tables: List<Table>.from(map['tables'].map((p) => Table.fromMap(p))),
-        );
-    }
+  factory TableList.fromMap(Map<String, dynamic> map) {
+    return TableList(
+      total: map['total'],
+      tables: List<Table>.from(map['tables'].map((p) => Table.fromMap(p))),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "tables": tables.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"total": total, "tables": tables.map((p) => p.toMap()).toList()};
+  }
 }
