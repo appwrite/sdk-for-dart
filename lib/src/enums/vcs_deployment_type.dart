bad4269130bfae90184e6bbb44ae40861d80ebf0
Jake Barnby <jakeb994@gmail.com>
2025-09-05 02:16:02 +1200

    Update version

--- lib/src/enums/vcs_deployment_type.dart
+++ lib/src/enums/vcs_deployment_type.dart
@@ -1,13 +1,15 @@
 part of '../../enums.dart';
 
 enum VCSDeploymentType {
-  branch(value: 'branch'),
-  commit(value: 'commit'),
-  tag(value: 'tag');
+    branch(value: 'branch'),
+    commit(value: 'commit'),
+    tag(value: 'tag');
 
-  const VCSDeploymentType({required this.value});
+    const VCSDeploymentType({
+        required this.value
+    });
 
-  final String value;
+    final String value;
 
-  String toJson() => value;
+    String toJson() => value;
 }
