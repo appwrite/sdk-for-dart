420540e3a7967b00b0aa99c2a3af49a0d99cd64a
abnegate <abnegate@users.noreply.github.com>
2025-09-23 03:40:57 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/column_boolean.dart
+++ lib/src/models/column_boolean.dart
@@ -2,70 +2,70 @@ part of '../../models.dart';
 
 /// ColumnBoolean
 class ColumnBoolean implements Model {
-    /// Column Key.
-    final String key;
+  /// Column Key.
+  final String key;
 
-    /// Column type.
-    final String type;
+  /// Column type.
+  final String type;
 
-    /// Column status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-    final String status;
+  /// Column status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+  final String status;
 
-    /// Error message. Displays error generated on failure of creating or deleting an column.
-    final String error;
+  /// Error message. Displays error generated on failure of creating or deleting an column.
+  final String error;
 
-    /// Is column required?
-    final bool xrequired;
+  /// Is column required?
+  final bool xrequired;
 
-    /// Is column an array?
-    final bool? array;
+  /// Is column an array?
+  final bool? array;
 
-    /// Column creation date in ISO 8601 format.
-    final String $createdAt;
+  /// Column creation date in ISO 8601 format.
+  final String $createdAt;
 
-    /// Column update date in ISO 8601 format.
-    final String $updatedAt;
+  /// Column update date in ISO 8601 format.
+  final String $updatedAt;
 
-    /// Default value for column when not provided. Cannot be set when column is required.
-    final bool? xdefault;
+  /// Default value for column when not provided. Cannot be set when column is required.
+  final bool? xdefault;
 
-    ColumnBoolean({
-        required this.key,
-        required this.type,
-        required this.status,
-        required this.error,
-        required this.xrequired,
-        this.array,
-        required this.$createdAt,
-        required this.$updatedAt,
-        this.xdefault,
-    });
+  ColumnBoolean({
+    required this.key,
+    required this.type,
+    required this.status,
+    required this.error,
+    required this.xrequired,
+    this.array,
+    required this.$createdAt,
+    required this.$updatedAt,
+    this.xdefault,
+  });
 
-    factory ColumnBoolean.fromMap(Map<String, dynamic> map) {
-        return ColumnBoolean(
-            key: map['key'].toString(),
-            type: map['type'].toString(),
-            status: map['status'].toString(),
-            error: map['error'].toString(),
-            xrequired: map['required'],
-            array: map['array'],
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            xdefault: map['default'],
-        );
-    }
+  factory ColumnBoolean.fromMap(Map<String, dynamic> map) {
+    return ColumnBoolean(
+      key: map['key'].toString(),
+      type: map['type'].toString(),
+      status: map['status'].toString(),
+      error: map['error'].toString(),
+      xrequired: map['required'],
+      array: map['array'],
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      xdefault: map['default'],
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "key": key,
-            "type": type,
-            "status": status,
-            "error": error,
-            "required": xrequired,
-            "array": array,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "default": xdefault,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "key": key,
+      "type": type,
+      "status": status,
+      "error": error,
+      "required": xrequired,
+      "array": array,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "default": xdefault,
+    };
+  }
 }
