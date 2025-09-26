fa1817080d893b78b0037b106d7dc56b2d267b2f
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-09-26 09:10:05 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/compression.dart
+++ lib/src/enums/compression.dart
@@ -1,15 +1,13 @@
 part of '../../enums.dart';
 
 enum Compression {
-    none(value: 'none'),
-    gzip(value: 'gzip'),
-    zstd(value: 'zstd');
+  none(value: 'none'),
+  gzip(value: 'gzip'),
+  zstd(value: 'zstd');
 
-    const Compression({
-        required this.value
-    });
+  const Compression({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
