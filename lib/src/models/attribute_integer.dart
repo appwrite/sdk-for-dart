0d2c4bcb4693c615167886677f71b88100f9c566
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-10-01 04:38:39 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/attribute_integer.dart
+++ lib/src/models/attribute_integer.dart
@@ -2,82 +2,84 @@ part of '../../models.dart';
 
 /// AttributeInteger
 class AttributeInteger implements Model {
-    /// Attribute Key.
-    final String key;
+  /// Attribute Key.
+  final String key;
 
-    /// Attribute type.
-    final String type;
+  /// Attribute type.
+  final String type;
 
-    /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-    final enums.AttributeStatus status;
+  /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+  final enums.AttributeStatus status;
 
-    /// Error message. Displays error generated on failure of creating or deleting an attribute.
-    final String error;
+  /// Error message. Displays error generated on failure of creating or deleting an attribute.
+  final String error;
 
-    /// Is attribute required?
-    final bool xrequired;
+  /// Is attribute required?
+  final bool xrequired;
 
-    /// Is attribute an array?
-    final bool? array;
+  /// Is attribute an array?
+  final bool? array;
 
-    /// Attribute creation date in ISO 8601 format.
-    final String $createdAt;
+  /// Attribute creation date in ISO 8601 format.
+  final String $createdAt;
 
-    /// Attribute update date in ISO 8601 format.
-    final String $updatedAt;
+  /// Attribute update date in ISO 8601 format.
+  final String $updatedAt;
 
-    /// Minimum value to enforce for new documents.
-    final int? min;
+  /// Minimum value to enforce for new documents.
+  final int? min;
 
-    /// Maximum value to enforce for new documents.
-    final int? max;
+  /// Maximum value to enforce for new documents.
+  final int? max;
 
-    /// Default value for attribute when not provided. Cannot be set when attribute is required.
-    final int? xdefault;
+  /// Default value for attribute when not provided. Cannot be set when attribute is required.
+  final int? xdefault;
 
-    AttributeInteger({
-        required this.key,
-        required this.type,
-        required this.status,
-        required this.error,
-        required this.xrequired,
-        this.array,
-        required this.$createdAt,
-        required this.$updatedAt,
-        this.min,
-        this.max,
-        this.xdefault,
-    });
+  AttributeInteger({
+    required this.key,
+    required this.type,
+    required this.status,
+    required this.error,
+    required this.xrequired,
+    this.array,
+    required this.$createdAt,
+    required this.$updatedAt,
+    this.min,
+    this.max,
+    this.xdefault,
+  });
 
-    factory AttributeInteger.fromMap(Map<String, dynamic> map) {
-        return AttributeInteger(
-            key: map['key'].toString(),
-            type: map['type'].toString(),
-            status: enums.AttributeStatus.values.firstWhere((e) => e.value == map['status']),
-            error: map['error'].toString(),
-            xrequired: map['required'],
-            array: map['array'],
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            min: map['min'],
-            max: map['max'],
-            xdefault: map['default'],
-        );
-    }
+  factory AttributeInteger.fromMap(Map<String, dynamic> map) {
+    return AttributeInteger(
+      key: map['key'].toString(),
+      type: map['type'].toString(),
+      status: enums.AttributeStatus.values.firstWhere(
+        (e) => e.value == map['status'],
+      ),
+      error: map['error'].toString(),
+      xrequired: map['required'],
+      array: map['array'],
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      min: map['min'],
+      max: map['max'],
+      xdefault: map['default'],
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "key": key,
-            "type": type,
-            "status": status.value,
-            "error": error,
-            "required": xrequired,
-            "array": array,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "min": min,
-            "max": max,
-            "default": xdefault,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "key": key,
+      "type": type,
+      "status": status.value,
+      "error": error,
+      "required": xrequired,
+      "array": array,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "min": min,
+      "max": max,
+      "default": xdefault,
+    };
+  }
 }
