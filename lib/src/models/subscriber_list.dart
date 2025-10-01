903a267fe4cdf03d3bac43f262d83444a50d8f14
root <chiragaggarwal5k@gmail.com>
2025-10-01 04:37:56 +0000

    regen

--- lib/src/models/subscriber_list.dart
+++ lib/src/models/subscriber_list.dart
@@ -2,27 +2,28 @@ part of '../../models.dart';
 
 /// Subscriber list
 class SubscriberList implements Model {
-  /// Total number of subscribers that matched your query.
-  final int total;
+    /// Total number of subscribers that matched your query.
+    final int total;
 
-  /// List of subscribers.
-  final List<Subscriber> subscribers;
+    /// List of subscribers.
+    final List<Subscriber> subscribers;
 
-  SubscriberList({required this.total, required this.subscribers});
+    SubscriberList({
+        required this.total,
+        required this.subscribers,
+    });
 
-  factory SubscriberList.fromMap(Map<String, dynamic> map) {
-    return SubscriberList(
-      total: map['total'],
-      subscribers: List<Subscriber>.from(
-        map['subscribers'].map((p) => Subscriber.fromMap(p)),
-      ),
-    );
-  }
+    factory SubscriberList.fromMap(Map<String, dynamic> map) {
+        return SubscriberList(
+            total: map['total'],
+            subscribers: List<Subscriber>.from(map['subscribers'].map((p) => Subscriber.fromMap(p))),
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {
-      "total": total,
-      "subscribers": subscribers.map((p) => p.toMap()).toList(),
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "subscribers": subscribers.map((p) => p.toMap()).toList(),
+        };
+    }
 }
