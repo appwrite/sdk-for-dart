1fe67ab4e503ebc373d716e0dc1d7cea5bda9a82
root <chiragaggarwal5k@gmail.com>
2025-10-08 04:18:09 +0000

    chore: regen

--- lib/src/models/attribute_float.dart
+++ lib/src/models/attribute_float.dart
@@ -2,84 +2,82 @@ part of '../../models.dart';
 
 /// AttributeFloat
 class AttributeFloat implements Model {
-  /// Attribute Key.
-  final String key;
+    /// Attribute Key.
+    final String key;
 
-  /// Attribute type.
-  final String type;
+    /// Attribute type.
+    final String type;
 
-  /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-  final enums.AttributeStatus status;
+    /// Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+    final enums.AttributeStatus status;
 
-  /// Error message. Displays error generated on failure of creating or deleting an attribute.
-  final String error;
+    /// Error message. Displays error generated on failure of creating or deleting an attribute.
+    final String error;
 
-  /// Is attribute required?
-  final bool xrequired;
+    /// Is attribute required?
+    final bool xrequired;
 
-  /// Is attribute an array?
-  final bool? array;
+    /// Is attribute an array?
+    final bool? array;
 
-  /// Attribute creation date in ISO 8601 format.
-  final String $createdAt;
+    /// Attribute creation date in ISO 8601 format.
+    final String $createdAt;
 
-  /// Attribute update date in ISO 8601 format.
-  final String $updatedAt;
+    /// Attribute update date in ISO 8601 format.
+    final String $updatedAt;
 
-  /// Minimum value to enforce for new documents.
-  final double? min;
+    /// Minimum value to enforce for new documents.
+    final double? min;
 
-  /// Maximum value to enforce for new documents.
-  final double? max;
+    /// Maximum value to enforce for new documents.
+    final double? max;
 
-  /// Default value for attribute when not provided. Cannot be set when attribute is required.
-  final double? xdefault;
+    /// Default value for attribute when not provided. Cannot be set when attribute is required.
+    final double? xdefault;
 
-  AttributeFloat({
-    required this.key,
-    required this.type,
-    required this.status,
-    required this.error,
-    required this.xrequired,
-    this.array,
-    required this.$createdAt,
-    required this.$updatedAt,
-    this.min,
-    this.max,
-    this.xdefault,
-  });
+    AttributeFloat({
+        required this.key,
+        required this.type,
+        required this.status,
+        required this.error,
+        required this.xrequired,
+        this.array,
+        required this.$createdAt,
+        required this.$updatedAt,
+        this.min,
+        this.max,
+        this.xdefault,
+    });
 
-  factory AttributeFloat.fromMap(Map<String, dynamic> map) {
-    return AttributeFloat(
-      key: map['key'].toString(),
-      type: map['type'].toString(),
-      status: enums.AttributeStatus.values.firstWhere(
-        (e) => e.value == map['status'],
-      ),
-      error: map['error'].toString(),
-      xrequired: map['required'],
-      array: map['array'],
-      $createdAt: map['\$createdAt'].toString(),
-      $updatedAt: map['\$updatedAt'].toString(),
-      min: map['min']?.toDouble(),
-      max: map['max']?.toDouble(),
-      xdefault: map['default']?.toDouble(),
-    );
-  }
+    factory AttributeFloat.fromMap(Map<String, dynamic> map) {
+        return AttributeFloat(
+            key: map['key'].toString(),
+            type: map['type'].toString(),
+            status: enums.AttributeStatus.values.firstWhere((e) => e.value == map['status']),
+            error: map['error'].toString(),
+            xrequired: map['required'],
+            array: map['array'],
+            $createdAt: map['\$createdAt'].toString(),
+            $updatedAt: map['\$updatedAt'].toString(),
+            min: map['min']?.toDouble(),
+            max: map['max']?.toDouble(),
+            xdefault: map['default']?.toDouble(),
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {
-      "key": key,
-      "type": type,
-      "status": status.value,
-      "error": error,
-      "required": xrequired,
-      "array": array,
-      "\$createdAt": $createdAt,
-      "\$updatedAt": $updatedAt,
-      "min": min,
-      "max": max,
-      "default": xdefault,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "key": key,
+            "type": type,
+            "status": status.value,
+            "error": error,
+            "required": xrequired,
+            "array": array,
+            "\$createdAt": $createdAt,
+            "\$updatedAt": $updatedAt,
+            "min": min,
+            "max": max,
+            "default": xdefault,
+        };
+    }
 }
--- lib/src/models/database.dart
+++ lib/src/models/database.dart
@@ -2,52 +2,52 @@ part of '../../models.dart';
 
 /// Database
 class Database implements Model {
-  /// Database ID.
-  final String $id;
+    /// Database ID.
+    final String $id;
 
-  /// Database name.
-  final String name;
+    /// Database name.
+    final String name;
 
-  /// Database creation date in ISO 8601 format.
-  final String $createdAt;
+    /// Database creation date in ISO 8601 format.
+    final String $createdAt;
 
-  /// Database update date in ISO 8601 format.
-  final String $updatedAt;
+    /// Database update date in ISO 8601 format.
+    final String $updatedAt;
 
-  /// If database is enabled. Can be &#039;enabled&#039; or &#039;disabled&#039;. When disabled, the database is inaccessible to users, but remains accessible to Server SDKs using API keys.
-  final bool enabled;
+    /// If database is enabled. Can be &#039;enabled&#039; or &#039;disabled&#039;. When disabled, the database is inaccessible to users, but remains accessible to Server SDKs using API keys.
+    final bool enabled;
 
-  /// Database type.
-  final enums.DatabaseType type;
+    /// Database type.
+    final enums.DatabaseType type;
 
-  Database({
-    required this.$id,
-    required this.name,
-    required this.$createdAt,
-    required this.$updatedAt,
-    required this.enabled,
-    required this.type,
-  });
+    Database({
+        required this.$id,
+        required this.name,
+        required this.$createdAt,
+        required this.$updatedAt,
+        required this.enabled,
+        required this.type,
+    });
 
-  factory Database.fromMap(Map<String, dynamic> map) {
-    return Database(
-      $id: map['\$id'].toString(),
-      name: map['name'].toString(),
-      $createdAt: map['\$createdAt'].toString(),
-      $updatedAt: map['\$updatedAt'].toString(),
-      enabled: map['enabled'],
-      type: enums.DatabaseType.values.firstWhere((e) => e.value == map['type']),
-    );
-  }
+    factory Database.fromMap(Map<String, dynamic> map) {
+        return Database(
+            $id: map['\$id'].toString(),
+            name: map['name'].toString(),
+            $createdAt: map['\$createdAt'].toString(),
+            $updatedAt: map['\$updatedAt'].toString(),
+            enabled: map['enabled'],
+            type: enums.DatabaseType.values.firstWhere((e) => e.value == map['type']),
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {
-      "\$id": $id,
-      "name": name,
-      "\$createdAt": $createdAt,
-      "\$updatedAt": $updatedAt,
-      "enabled": enabled,
-      "type": type.value,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "\$id": $id,
+            "name": name,
+            "\$createdAt": $createdAt,
+            "\$updatedAt": $updatedAt,
+            "enabled": enabled,
+            "type": type.value,
+        };
+    }
 }
