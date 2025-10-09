dd7366a13f76d3b55d468ff5fe327ae040418e59
abnegate <abnegate@users.noreply.github.com>
2025-10-09 07:24:51 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/column_polygon.dart
+++ lib/src/models/column_polygon.dart
@@ -2,70 +2,72 @@ part of '../../models.dart';
 
 /// ColumnPolygon
 class ColumnPolygon implements Model {
-    /// Column Key.
-    final String key;
+  /// Column Key.
+  final String key;
 
-    /// Column type.
-    final String type;
+  /// Column type.
+  final String type;
 
-    /// Column status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
-    final enums.ColumnStatus status;
+  /// Column status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+  final enums.ColumnStatus status;
 
-    /// Error message. Displays error generated on failure of creating or deleting an column.
-    final String error;
+  /// Error message. Displays error generated on failure of creating or deleting an column.
+  final String error;
 
-    /// Is column required?
-    final bool xrequired;
+  /// Is column required?
+  final bool xrequired;
 
-    /// Is column an array?
-    final bool? array;
+  /// Is column an array?
+  final bool? array;
 
-    /// Column creation date in ISO 8601 format.
-    final String $createdAt;
+  /// Column creation date in ISO 8601 format.
+  final String $createdAt;
 
-    /// Column update date in ISO 8601 format.
-    final String $updatedAt;
+  /// Column update date in ISO 8601 format.
+  final String $updatedAt;
 
-    /// Default value for column when not provided. Cannot be set when column is required.
-    final List? xdefault;
+  /// Default value for column when not provided. Cannot be set when column is required.
+  final List? xdefault;
 
-    ColumnPolygon({
-        required this.key,
-        required this.type,
-        required this.status,
-        required this.error,
-        required this.xrequired,
-        this.array,
-        required this.$createdAt,
-        required this.$updatedAt,
-        this.xdefault,
-    });
+  ColumnPolygon({
+    required this.key,
+    required this.type,
+    required this.status,
+    required this.error,
+    required this.xrequired,
+    this.array,
+    required this.$createdAt,
+    required this.$updatedAt,
+    this.xdefault,
+  });
 
-    factory ColumnPolygon.fromMap(Map<String, dynamic> map) {
-        return ColumnPolygon(
-            key: map['key'].toString(),
-            type: map['type'].toString(),
-            status: enums.ColumnStatus.values.firstWhere((e) => e.value == map['status']),
-            error: map['error'].toString(),
-            xrequired: map['required'],
-            array: map['array'],
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            xdefault: List.from(map['default'] ?? []),
-        );
-    }
+  factory ColumnPolygon.fromMap(Map<String, dynamic> map) {
+    return ColumnPolygon(
+      key: map['key'].toString(),
+      type: map['type'].toString(),
+      status: enums.ColumnStatus.values.firstWhere(
+        (e) => e.value == map['status'],
+      ),
+      error: map['error'].toString(),
+      xrequired: map['required'],
+      array: map['array'],
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      xdefault: List.from(map['default'] ?? []),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "key": key,
-            "type": type,
-            "status": status.value,
-            "error": error,
-            "required": xrequired,
-            "array": array,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "default": xdefault,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "key": key,
+      "type": type,
+      "status": status.value,
+      "error": error,
+      "required": xrequired,
+      "array": array,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "default": xdefault,
+    };
+  }
 }
