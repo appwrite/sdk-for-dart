bfece00f070f987ce46e20830e9f5d78fb37cc3e
root <chiragaggarwal5k@gmail.com>
2025-12-01 13:56:04 +0000

    update examples

--- lib/src/models/table.dart
+++ lib/src/models/table.dart
@@ -2,78 +2,77 @@ part of '../../models.dart';
 
 /// Table
 class Table implements Model {
-  /// Table ID.
-  final String $id;
+    /// Table ID.
+    final String $id;
 
-  /// Table creation date in ISO 8601 format.
-  final String $createdAt;
+    /// Table creation date in ISO 8601 format.
+    final String $createdAt;
 
-  /// Table update date in ISO 8601 format.
-  final String $updatedAt;
+    /// Table update date in ISO 8601 format.
+    final String $updatedAt;
 
-  /// Table permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
-  final List<String> $permissions;
+    /// Table permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
+    final List<String> $permissions;
 
-  /// Database ID.
-  final String databaseId;
+    /// Database ID.
+    final String databaseId;
 
-  /// Table name.
-  final String name;
+    /// Table name.
+    final String name;
 
-  /// Table enabled. Can be &#039;enabled&#039; or &#039;disabled&#039;. When disabled, the table is inaccessible to users, but remains accessible to Server SDKs using API keys.
-  final bool enabled;
+    /// Table enabled. Can be &#039;enabled&#039; or &#039;disabled&#039;. When disabled, the table is inaccessible to users, but remains accessible to Server SDKs using API keys.
+    final bool enabled;
 
-  /// Whether row-level permissions are enabled. [Learn more about permissions](https://appwrite.io/docs/permissions).
-  final bool rowSecurity;
+    /// Whether row-level permissions are enabled. [Learn more about permissions](https://appwrite.io/docs/permissions).
+    final bool rowSecurity;
 
-  /// Table columns.
-  final List columns;
+    /// Table columns.
+    final List columns;
 
-  /// Table indexes.
-  final List<ColumnIndex> indexes;
+    /// Table indexes.
+    final List<ColumnIndex> indexes;
 
-  Table({
-    required this.$id,
-    required this.$createdAt,
-    required this.$updatedAt,
-    required this.$permissions,
-    required this.databaseId,
-    required this.name,
-    required this.enabled,
-    required this.rowSecurity,
-    required this.columns,
-    required this.indexes,
-  });
+    Table({
+        required this.$id,
+        required this.$createdAt,
+        required this.$updatedAt,
+        required this.$permissions,
+        required this.databaseId,
+        required this.name,
+        required this.enabled,
+        required this.rowSecurity,
+        required this.columns,
+        required this.indexes,
+    });
 
-  factory Table.fromMap(Map<String, dynamic> map) {
-    return Table(
-      $id: map['\$id'].toString(),
-      $createdAt: map['\$createdAt'].toString(),
-      $updatedAt: map['\$updatedAt'].toString(),
-      $permissions: List.from(map['\$permissions'] ?? []),
-      databaseId: map['databaseId'].toString(),
-      name: map['name'].toString(),
-      enabled: map['enabled'],
-      rowSecurity: map['rowSecurity'],
-      columns: List.from(map['columns'] ?? []),
-      indexes: List<ColumnIndex>.from(
-          map['indexes'].map((p) => ColumnIndex.fromMap(p))),
-    );
-  }
+    factory Table.fromMap(Map<String, dynamic> map) {
+        return Table(
+            $id: map['\$id'].toString(),
+            $createdAt: map['\$createdAt'].toString(),
+            $updatedAt: map['\$updatedAt'].toString(),
+            $permissions: List.from(map['\$permissions'] ?? []),
+            databaseId: map['databaseId'].toString(),
+            name: map['name'].toString(),
+            enabled: map['enabled'],
+            rowSecurity: map['rowSecurity'],
+            columns: List.from(map['columns'] ?? []),
+            indexes: List<ColumnIndex>.from(map['indexes'].map((p) => ColumnIndex.fromMap(p))),
+        );
+    }
 
-  @override
-  Map<String, dynamic> toMap() {
-    return {
-      "\$id": $id,
-      "\$createdAt": $createdAt,
-      "\$updatedAt": $updatedAt,
-      "\$permissions": $permissions,
-      "databaseId": databaseId,
-      "name": name,
-      "enabled": enabled,
-      "rowSecurity": rowSecurity,
-      "columns": columns,
-      "indexes": indexes.map((p) => p.toMap()).toList(),
-    };
-  }
+    @override
+    Map<String, dynamic> toMap() {
+        return {
+            "\$id": $id,
+            "\$createdAt": $createdAt,
+            "\$updatedAt": $updatedAt,
+            "\$permissions": $permissions,
+            "databaseId": databaseId,
+            "name": name,
+            "enabled": enabled,
+            "rowSecurity": rowSecurity,
+            "columns": columns,
+            "indexes": indexes.map((p) => p.toMap()).toList(),
+        };
+    }
 }
--- test/services/health_test.dart
+++ test/services/health_test.dart
@@ -24,8 +24,7 @@ class MockClient extends Mock implements Client {
 
   @override
   Future<String?> webAuth(Uri url) async {
-    return super
-        .noSuchMethod(Invocation.method(#webAuth, [url]), returnValue: 'done');
+    return super.noSuchMethod(Invocation.method(#webAuth, [url]), returnValue: 'done');
   }
 
   @override
@@ -37,329 +36,392 @@ class MockClient extends Mock implements Client {
     Map<String, String>? headers,
     Function(UploadProgress)? onProgress,
   }) async {
-    return super.noSuchMethod(
-        Invocation.method(
-            #chunkedUpload, [path, params, paramName, idParamName, headers]),
-        returnValue: Response(data: {}));
+    return super.noSuchMethod(Invocation.method(#chunkedUpload, [path, params, paramName, idParamName, headers]), returnValue: Response(data: {}));
   }
 }
 
 void main() {
-  group('Health test', () {
-    late MockClient client;
-    late Health health;
+    group('Health test', () {
+        late MockClient client;
+        late Health health;
 
-    setUp(() {
-      client = MockClient();
-      health = Health(client);
+        setUp(() {
+            client = MockClient();
+            health = Health(client);
+        });
+
+        test('test method get()', () async {
+            final Map<String, dynamic> data = {
+                'name': 'database',
+                'ping': 128,
+                'status': 'pass',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.get(
+            );
+            expect(response, isA<models.HealthStatus>());
+
+        });
+
+        test('test method getAntivirus()', () async {
+            final Map<String, dynamic> data = {
+                'version': '1.0.0',
+                'status': 'online',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getAntivirus(
+            );
+            expect(response, isA<models.HealthAntivirus>());
+
+        });
+
+        test('test method getCache()', () async {
+            final Map<String, dynamic> data = {
+                'name': 'database',
+                'ping': 128,
+                'status': 'pass',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getCache(
+            );
+            expect(response, isA<models.HealthStatus>());
+
+        });
+
+        test('test method getCertificate()', () async {
+            final Map<String, dynamic> data = {
+                'name': '/CN=www.google.com',
+                'subjectSN': '',
+                'issuerOrganisation': '',
+                'validFrom': '1704200998',
+                'validTo': '1711458597',
+                'signatureTypeSN': 'RSA-SHA256',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getCertificate(
+            );
+            expect(response, isA<models.HealthCertificate>());
+
+        });
+
+        test('test method getDB()', () async {
+            final Map<String, dynamic> data = {
+                'name': 'database',
+                'ping': 128,
+                'status': 'pass',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getDB(
+            );
+            expect(response, isA<models.HealthStatus>());
+
+        });
+
+        test('test method getPubSub()', () async {
+            final Map<String, dynamic> data = {
+                'name': 'database',
+                'ping': 128,
+                'status': 'pass',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getPubSub(
+            );
+            expect(response, isA<models.HealthStatus>());
+
+        });
+
+        test('test method getQueueBuilds()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueBuilds(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueCertificates()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueCertificates(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueDatabases()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueDatabases(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueDeletes()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueDeletes(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getFailedJobs()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getFailedJobs(
+                name: enums.Name.v1Database,
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueFunctions()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueFunctions(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueLogs()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueLogs(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueMails()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueMails(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueMessaging()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueMessaging(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueMigrations()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueMigrations(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueStatsResources()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueStatsResources(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueUsage()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueUsage(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getQueueWebhooks()', () async {
+            final Map<String, dynamic> data = {
+                'size': 8,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getQueueWebhooks(
+            );
+            expect(response, isA<models.HealthQueue>());
+
+        });
+
+        test('test method getStorage()', () async {
+            final Map<String, dynamic> data = {
+                'name': 'database',
+                'ping': 128,
+                'status': 'pass',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getStorage(
+            );
+            expect(response, isA<models.HealthStatus>());
+
+        });
+
+        test('test method getStorageLocal()', () async {
+            final Map<String, dynamic> data = {
+                'name': 'database',
+                'ping': 128,
+                'status': 'pass',};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getStorageLocal(
+            );
+            expect(response, isA<models.HealthStatus>());
+
+        });
+
+        test('test method getTime()', () async {
+            final Map<String, dynamic> data = {
+                'remoteTime': 1639490751,
+                'localTime': 1639490844,
+                'diff': 93,};
+
+
+            when(client.call(
+                HttpMethod.get,
+            )).thenAnswer((_) async => Response(data: data));
+
+
+            final response = await health.getTime(
+            );
+            expect(response, isA<models.HealthTime>());
+
+        });
+
     });
-
-    test('test method get()', () async {
-      final Map<String, dynamic> data = {
-        'name': 'database',
-        'ping': 128,
-        'status': 'pass',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.get();
-      expect(response, isA<models.HealthStatus>());
-    });
-
-    test('test method getAntivirus()', () async {
-      final Map<String, dynamic> data = {
-        'version': '1.0.0',
-        'status': 'online',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getAntivirus();
-      expect(response, isA<models.HealthAntivirus>());
-    });
-
-    test('test method getCache()', () async {
-      final Map<String, dynamic> data = {
-        'name': 'database',
-        'ping': 128,
-        'status': 'pass',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getCache();
-      expect(response, isA<models.HealthStatus>());
-    });
-
-    test('test method getCertificate()', () async {
-      final Map<String, dynamic> data = {
-        'name': '/CN=www.google.com',
-        'subjectSN': '',
-        'issuerOrganisation': '',
-        'validFrom': '1704200998',
-        'validTo': '1711458597',
-        'signatureTypeSN': 'RSA-SHA256',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getCertificate();
-      expect(response, isA<models.HealthCertificate>());
-    });
-
-    test('test method getDB()', () async {
-      final Map<String, dynamic> data = {
-        'name': 'database',
-        'ping': 128,
-        'status': 'pass',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getDB();
-      expect(response, isA<models.HealthStatus>());
-    });
-
-    test('test method getPubSub()', () async {
-      final Map<String, dynamic> data = {
-        'name': 'database',
-        'ping': 128,
-        'status': 'pass',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getPubSub();
-      expect(response, isA<models.HealthStatus>());
-    });
-
-    test('test method getQueueBuilds()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueBuilds();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueCertificates()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueCertificates();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueDatabases()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueDatabases();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueDeletes()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueDeletes();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getFailedJobs()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getFailedJobs(
-        name: enums.Name.v1Database,
-      );
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueFunctions()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueFunctions();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueLogs()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueLogs();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueMails()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueMails();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueMessaging()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueMessaging();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueMigrations()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueMigrations();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueStatsResources()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueStatsResources();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueUsage()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueUsage();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getQueueWebhooks()', () async {
-      final Map<String, dynamic> data = {
-        'size': 8,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getQueueWebhooks();
-      expect(response, isA<models.HealthQueue>());
-    });
-
-    test('test method getStorage()', () async {
-      final Map<String, dynamic> data = {
-        'name': 'database',
-        'ping': 128,
-        'status': 'pass',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getStorage();
-      expect(response, isA<models.HealthStatus>());
-    });
-
-    test('test method getStorageLocal()', () async {
-      final Map<String, dynamic> data = {
-        'name': 'database',
-        'ping': 128,
-        'status': 'pass',
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getStorageLocal();
-      expect(response, isA<models.HealthStatus>());
-    });
-
-    test('test method getTime()', () async {
-      final Map<String, dynamic> data = {
-        'remoteTime': 1639490751,
-        'localTime': 1639490844,
-        'diff': 93,
-      };
-
-      when(client.call(
-        HttpMethod.get,
-      )).thenAnswer((_) async => Response(data: data));
-
-      final response = await health.getTime();
-      expect(response, isA<models.HealthTime>());
-    });
-  });
 }
