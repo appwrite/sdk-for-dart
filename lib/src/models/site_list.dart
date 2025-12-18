117610202698c4cc16cd61a86ea43835cef17529
Jake Barnby <jakeb994@gmail.com>
2025-12-18 20:35:45 +1300

    Update changelog

--- lib/src/models/site_list.dart
+++ lib/src/models/site_list.dart
@@ -2,29 +2,29 @@ part of '../../models.dart';
 
 /// Sites List
 class SiteList implements Model {
-  /// Total number of sites that matched your query.
-  final int total;
+    /// Total number of sites that matched your query.
+    final int total;
 
-  /// List of sites.
-  final List<Site> sites;
+    /// List of sites.
+    final List<Site> sites;
 
-  SiteList({
-    required this.total,
-    required this.sites,
-  });
+    SiteList({
+        required this.total,
+        required this.sites,
+    });
 
-  factory SiteList.fromMap(Map<String, dynamic> map) {
-    return SiteList(
-      total: map['total'],
-      sites: List<Site>.from(map['sites'].map((p) => Site.fromMap(p))),
-    );
-  }
+    factory SiteList.fromMap(Map<String, dynamic> map) {
+        return SiteList(
+            total: map['total'],
+            sites: List<Site>.from(map['sites'].map((p) => Site.fromMap(p))),
+        );
+    }
 
-  @override
-  Map<String, dynamic> toMap() {
-    return {
-      "total": total,
-      "sites": sites.map((p) => p.toMap()).toList(),
-    };
-  }
+    @override
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "sites": sites.map((p) => p.toMap()).toList(),
+        };
+    }
 }
