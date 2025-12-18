33df2db0cea15901e2193bded55e3fd15c13249e
abnegate <abnegate@users.noreply.github.com>
2025-12-18 07:36:14 +0000

    Commit from GitHub Actions (Format and push)

--- test/src/models/runtime_test.dart
+++ test/src/models/runtime_test.dart
@@ -18,14 +18,14 @@ void main() {
       final map = model.toMap();
       final result = Runtime.fromMap(map);
 
-            expect(result.$id, 'python-3.8');
-                  expect(result.key, 'python');
-                  expect(result.name, 'Python');
-                  expect(result.version, '3.8');
-                  expect(result.base, 'python:3.8-alpine');
-                  expect(result.image, 'appwrite\/runtime-for-python:3.8');
-                  expect(result.logo, 'python.png');
-                  expect(result.supports, []);
-          });
+      expect(result.$id, 'python-3.8');
+      expect(result.key, 'python');
+      expect(result.name, 'Python');
+      expect(result.version, '3.8');
+      expect(result.base, 'python:3.8-alpine');
+      expect(result.image, 'appwrite\/runtime-for-python:3.8');
+      expect(result.logo, 'python.png');
+      expect(result.supports, []);
+    });
   });
 }
