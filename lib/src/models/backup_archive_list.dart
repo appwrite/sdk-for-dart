2d8fadef55582c9a3a22a374b322cc5d07f210fa
Darshan <itznotabug@gmail.com>
2026-01-14 07:41:27 +0000

    updates: sync as per latest cloud specs

--- lib/src/models/backup_archive_list.dart
+++ lib/src/models/backup_archive_list.dart
@@ -2,30 +2,29 @@ part of '../../models.dart';
 
 /// Backup archive list
 class BackupArchiveList implements Model {
-  /// Total number of archives that matched your query.
-  final int total;
+    /// Total number of archives that matched your query.
+    final int total;
 
-  /// List of archives.
-  final List<BackupArchive> archives;
+    /// List of archives.
+    final List<BackupArchive> archives;
 
-  BackupArchiveList({
-    required this.total,
-    required this.archives,
-  });
+    BackupArchiveList({
+        required this.total,
+        required this.archives,
+    });
 
-  factory BackupArchiveList.fromMap(Map<String, dynamic> map) {
-    return BackupArchiveList(
-      total: map['total'],
-      archives: List<BackupArchive>.from(
-          map['archives'].map((p) => BackupArchive.fromMap(p))),
-    );
-  }
+    factory BackupArchiveList.fromMap(Map<String, dynamic> map) {
+        return BackupArchiveList(
+            total: map['total'],
+            archives: List<BackupArchive>.from(map['archives'].map((p) => BackupArchive.fromMap(p))),
+        );
+    }
 
-  @override
-  Map<String, dynamic> toMap() {
-    return {
-      "total": total,
-      "archives": archives.map((p) => p.toMap()).toList(),
-    };
-  }
+    @override
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "archives": archives.map((p) => p.toMap()).toList(),
+        };
+    }
 }
