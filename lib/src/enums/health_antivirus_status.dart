d8ad723a6b188e251d858035ea3b986c00c59c28
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-02-12 08:38:40 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/health_antivirus_status.dart
+++ lib/src/enums/health_antivirus_status.dart
@@ -1,15 +1,13 @@
 part of '../../enums.dart';
 
 enum HealthAntivirusStatus {
-    disabled(value: 'disabled'),
-    offline(value: 'offline'),
-    online(value: 'online');
+  disabled(value: 'disabled'),
+  offline(value: 'offline'),
+  online(value: 'online');
 
-    const HealthAntivirusStatus({
-        required this.value
-    });
+  const HealthAntivirusStatus({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
