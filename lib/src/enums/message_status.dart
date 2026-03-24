af9f2e81300debf16ccf8591f5ba63409887d64c
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-03-24 13:22:56 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/message_status.dart
+++ lib/src/enums/message_status.dart
@@ -1,17 +1,15 @@
 part of '../../enums.dart';
 
 enum MessageStatus {
-    draft(value: 'draft'),
-    processing(value: 'processing'),
-    scheduled(value: 'scheduled'),
-    sent(value: 'sent'),
-    failed(value: 'failed');
+  draft(value: 'draft'),
+  processing(value: 'processing'),
+  scheduled(value: 'scheduled'),
+  sent(value: 'sent'),
+  failed(value: 'failed');
 
-    const MessageStatus({
-        required this.value
-    });
+  const MessageStatus({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
