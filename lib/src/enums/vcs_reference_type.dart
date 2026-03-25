e67e78a39c331f7cbc3b37a1f50c7062dcb09fc6
root <chiragaggarwal5k@gmail.com>
2026-03-25 06:44:37 +0000

    chore: update Dart SDK to 22.0.0

--- lib/src/enums/vcs_reference_type.dart
+++ lib/src/enums/vcs_reference_type.dart
@@ -1,13 +1,14 @@
 part of '../../enums.dart';
 
 enum VCSReferenceType {
-  branch(value: 'branch'),
-  commit(value: 'commit'),
-  tag(value: 'tag');
+    branch(value: 'branch'),
+    commit(value: 'commit');
 
-  const VCSReferenceType({required this.value});
+    const VCSReferenceType({
+        required this.value
+    });
 
-  final String value;
+    final String value;
 
-  String toJson() => value;
+    String toJson() => value;
 }
