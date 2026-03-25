b7fb8ace8c60577cc9ae0b3c55f18e78b8719a00
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-03-25 06:45:05 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/databases_index_type.dart
+++ lib/src/enums/databases_index_type.dart
@@ -1,16 +1,14 @@
 part of '../../enums.dart';
 
 enum DatabasesIndexType {
-    key(value: 'key'),
-    fulltext(value: 'fulltext'),
-    unique(value: 'unique'),
-    spatial(value: 'spatial');
+  key(value: 'key'),
+  fulltext(value: 'fulltext'),
+  unique(value: 'unique'),
+  spatial(value: 'spatial');
 
-    const DatabasesIndexType({
-        required this.value
-    });
+  const DatabasesIndexType({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
--- lib/src/enums/vcs_reference_type.dart
+++ lib/src/enums/vcs_reference_type.dart
@@ -1,14 +1,12 @@
 part of '../../enums.dart';
 
 enum VCSReferenceType {
-    branch(value: 'branch'),
-    commit(value: 'commit');
+  branch(value: 'branch'),
+  commit(value: 'commit');
 
-    const VCSReferenceType({
-        required this.value
-    });
+  const VCSReferenceType({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
