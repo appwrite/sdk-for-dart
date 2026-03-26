40d56c3519919ff363c2695888aa1f9ea51ff3c2
root <chiragaggarwal5k@gmail.com>
2026-03-26 05:07:38 +0000

    chore: update Dart SDK to 22.0.0

--- lib/src/enums/smtp_encryption.dart
+++ lib/src/enums/smtp_encryption.dart
@@ -1,13 +1,15 @@
 part of '../../enums.dart';
 
 enum SmtpEncryption {
-  none(value: 'none'),
-  ssl(value: 'ssl'),
-  tls(value: 'tls');
+    none(value: 'none'),
+    ssl(value: 'ssl'),
+    tls(value: 'tls');
 
-  const SmtpEncryption({required this.value});
+    const SmtpEncryption({
+        required this.value
+    });
 
-  final String value;
+    final String value;
 
-  String toJson() => value;
+    String toJson() => value;
 }
