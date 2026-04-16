924ef7067a2b56f2abfe4ed068cbd11aaae335c3
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-04-16 12:17:03 +0000

    Commit from GitHub Actions (Format and push)

--- test/src/models/framework_list_test.dart
+++ test/src/models/framework_list_test.dart
@@ -12,8 +12,8 @@ void main() {
       final map = model.toMap();
       final result = FrameworkList.fromMap(map);
 
-            expect(result.total, 5);
-                  expect(result.frameworks, []);
-          });
+      expect(result.total, 5);
+      expect(result.frameworks, []);
+    });
   });
 }
