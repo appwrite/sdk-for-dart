5b3f93c580a4dbc628718215fd93009b7bd80e9d
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-05-07 17:29:29 +0530

    chore: regenerate sdk examples with safe oauth placeholders

--- lib/services/activities.dart
+++ lib/services/activities.dart
@@ -1,37 +1,45 @@
 part of '../dart_appwrite.dart';
 
 class Activities extends Service {
-  Activities(super.client);
+    Activities(super.client);
 
   /// List all events for selected filters.
-  Future<models.ActivityEventList> listEvents({String? queries}) async {
-    final String apiPath = '/activities/events';
+    Future<models.ActivityEventList> listEvents({String? queries}) async {
+        final String apiPath = '/activities/events';
 
     final Map<String, dynamic> apiParams = {
       if (queries != null) 'queries': queries,
+
+      
     };
 
-    final Map<String, String> apiHeaders = {};
+    final Map<String, String> apiHeaders = {
+      
+    };
 
-    final res = await client.call(HttpMethod.get,
-        path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
     return models.ActivityEventList.fromMap(res.data);
-  }
+
+    }
 
   /// Get event by ID.
-  ///
-  Future<models.ActivityEvent> getEvent({required String eventId}) async {
-    final String apiPath =
-        '/activities/events/{eventId}'.replaceAll('{eventId}', eventId);
+  /// 
+    Future<models.ActivityEvent> getEvent({required String eventId}) async {
+        final String apiPath = '/activities/events/{eventId}'.replaceAll('{eventId}', eventId);
 
-    final Map<String, dynamic> apiParams = {};
+    final Map<String, dynamic> apiParams = {
+      
+      
+    };
 
-    final Map<String, String> apiHeaders = {};
+    final Map<String, String> apiHeaders = {
+      
+    };
 
-    final res = await client.call(HttpMethod.get,
-        path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
     return models.ActivityEvent.fromMap(res.data);
-  }
+
+    }
 }
