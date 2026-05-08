5fd85f7091c210dab82b2d94847eeedf697dfcb4
ArnabChatterjee20k <ArnabChatterjee20k@users.noreply.github.com>
2026-05-08 11:00:59 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/o_auth2_box.dart
+++ lib/src/models/o_auth2_box.dart
@@ -2,41 +2,41 @@ part of '../../models.dart';
 
 /// OAuth2Box
 class OAuth2Box implements Model {
-    /// OAuth2 provider ID.
-    final String $id;
+  /// OAuth2 provider ID.
+  final String $id;
 
-    /// OAuth2 provider is active and can be used to create sessions.
-    final bool enabled;
+  /// OAuth2 provider is active and can be used to create sessions.
+  final bool enabled;
 
-    /// Box OAuth2 client ID.
-    final String clientId;
+  /// Box OAuth2 client ID.
+  final String clientId;
 
-    /// Box OAuth2 client secret.
-    final String clientSecret;
+  /// Box OAuth2 client secret.
+  final String clientSecret;
 
-    OAuth2Box({
-        required this.$id,
-        required this.enabled,
-        required this.clientId,
-        required this.clientSecret,
-    });
+  OAuth2Box({
+    required this.$id,
+    required this.enabled,
+    required this.clientId,
+    required this.clientSecret,
+  });
 
-    factory OAuth2Box.fromMap(Map<String, dynamic> map) {
-        return OAuth2Box(
-            $id: map['\$id'].toString(),
-            enabled: map['enabled'],
-            clientId: map['clientId'].toString(),
-            clientSecret: map['clientSecret'].toString(),
-        );
-    }
+  factory OAuth2Box.fromMap(Map<String, dynamic> map) {
+    return OAuth2Box(
+      $id: map['\$id'].toString(),
+      enabled: map['enabled'],
+      clientId: map['clientId'].toString(),
+      clientSecret: map['clientSecret'].toString(),
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "\$id": $id,
-            "enabled": enabled,
-            "clientId": clientId,
-            "clientSecret": clientSecret,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "\$id": $id,
+      "enabled": enabled,
+      "clientId": clientId,
+      "clientSecret": clientSecret,
+    };
+  }
 }
