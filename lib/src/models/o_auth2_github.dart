859d8c2056be80b04578fca4b1fb74709f9cec33
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-07-13 06:42:24 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/o_auth2_github.dart
+++ lib/src/models/o_auth2_github.dart
@@ -2,41 +2,41 @@ part of '../../models.dart';
 
 /// OAuth2GitHub
 class OAuth2Github implements Model {
-    /// OAuth2 provider ID.
-    final String $id;
+  /// OAuth2 provider ID.
+  final String $id;
 
-    /// OAuth2 provider is active and can be used to create sessions.
-    final bool enabled;
+  /// OAuth2 provider is active and can be used to create sessions.
+  final bool enabled;
 
-    /// GitHub OAuth2 client ID. For GitHub Apps, use the &quot;App ID&quot; when both an App ID and client ID are available.
-    final String clientId;
+  /// GitHub OAuth2 client ID. For GitHub Apps, use the &quot;App ID&quot; when both an App ID and client ID are available.
+  final String clientId;
 
-    /// GitHub OAuth2 client secret.
-    final String clientSecret;
+  /// GitHub OAuth2 client secret.
+  final String clientSecret;
 
-    OAuth2Github({
-        required this.$id,
-        required this.enabled,
-        required this.clientId,
-        required this.clientSecret,
-    });
+  OAuth2Github({
+    required this.$id,
+    required this.enabled,
+    required this.clientId,
+    required this.clientSecret,
+  });
 
-    factory OAuth2Github.fromMap(Map<String, dynamic> map) {
-        return OAuth2Github(
-            $id: map['\$id'].toString(),
-            enabled: map['enabled'],
-            clientId: map['clientId'].toString(),
-            clientSecret: map['clientSecret'].toString(),
-        );
-    }
+  factory OAuth2Github.fromMap(Map<String, dynamic> map) {
+    return OAuth2Github(
+      $id: map['\$id'].toString(),
+      enabled: map['enabled'],
+      clientId: map['clientId'].toString(),
+      clientSecret: map['clientSecret'].toString(),
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "\$id": $id,
-            "enabled": enabled,
-            "clientId": clientId,
-            "clientSecret": clientSecret,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "\$id": $id,
+      "enabled": enabled,
+      "clientId": clientId,
+      "clientSecret": clientSecret,
+    };
+  }
 }
--- test/src/models/activity_event_test.dart
+++ test/src/models/activity_event_test.dart
@@ -15,7 +15,8 @@ void main() {
         resourceId: '610fc2f985ee0',
         resource: 'collections/610fc2f985ee0',
         event: 'account.sessions.create',
-        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+        userAgent:
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
         ip: '127.0.0.1',
         mode: 'admin',
         country: 'US',
@@ -28,24 +29,25 @@ void main() {
       final map = model.toMap();
       final result = ActivityEvent.fromMap(map);
 
-            expect(result.$id, '5e5ea5c16897e');
-                  expect(result.actorType, 'user');
-                  expect(result.actorId, '610fc2f985ee0');
-                  expect(result.actorEmail, 'john@appwrite.io');
-                  expect(result.actorName, 'John Doe');
-                  expect(result.resourceParent, 'database/ID');
-                  expect(result.resourceType, 'collection');
-                  expect(result.resourceId, '610fc2f985ee0');
-                  expect(result.resource, 'collections/610fc2f985ee0');
-                  expect(result.event, 'account.sessions.create');
-                  expect(result.userAgent, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36');
-                  expect(result.ip, '127.0.0.1');
-                  expect(result.mode, 'admin');
-                  expect(result.country, 'US');
-                  expect(result.time, '2020-10-15T06:38:00.000+00:00');
-                  expect(result.projectId, '610fc2f985ee0');
-                  expect(result.teamId, '610fc2f985ee0');
-                  expect(result.hostname, 'appwrite.io');
-          });
+      expect(result.$id, '5e5ea5c16897e');
+      expect(result.actorType, 'user');
+      expect(result.actorId, '610fc2f985ee0');
+      expect(result.actorEmail, 'john@appwrite.io');
+      expect(result.actorName, 'John Doe');
+      expect(result.resourceParent, 'database/ID');
+      expect(result.resourceType, 'collection');
+      expect(result.resourceId, '610fc2f985ee0');
+      expect(result.resource, 'collections/610fc2f985ee0');
+      expect(result.event, 'account.sessions.create');
+      expect(result.userAgent,
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36');
+      expect(result.ip, '127.0.0.1');
+      expect(result.mode, 'admin');
+      expect(result.country, 'US');
+      expect(result.time, '2020-10-15T06:38:00.000+00:00');
+      expect(result.projectId, '610fc2f985ee0');
+      expect(result.teamId, '610fc2f985ee0');
+      expect(result.hostname, 'appwrite.io');
+    });
   });
 }
